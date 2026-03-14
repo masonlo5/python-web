4a055b9acb91c4dec9137cd09d21bebc80c44057
masonlo5 <sophie504504@gmail.com>
2026-03-14 13:42:00 +0800

    新增 prj02.py，實現顯示與清除訊息的功能

--- class1/prj02.py
+++ class1/prj02.py
@@ -7,6 +7,11 @@ from tkinter import *
 def say_fun():
     # 顯示訊息
     print("Hello, World!")
+    display.config(text="Hello, World!", fg="blue", bg="yellow")
+
+
+def clear_fun():
+    display.config(text="")
 
 
 #######################建立視窗########################
@@ -18,15 +23,16 @@ window.title("我的第一個GUI程式")
 
 #######################建立按鈕########################
 # 建立按鈕，並指定按下按鈕時要執行的函數
-button = Button(window, text="點我", command=say_fun)
+button = Button(window, text="Show screen", command=say_fun)
 # 將按鈕放置在視窗中
 button.pack()
-
+button2 = Button(window, text="Clear screen", command=clear_fun)
+button2.pack()
 
 #########################建立標籤########################
 # 創建一個標籤，顯示一些文字
 # Label參數說明： (視窗名稱，顯示的文字，文字顏色，背景顏色)
-display = Label(window, text="歡迎使用我的GUI程式！", fg="blue", bg="yellow")
+display = Label(window, text="")
 # 將標籤放置在視窗中
 display.pack()
 
